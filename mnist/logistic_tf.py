Fix issue with SGD underflowing making every softmax vector a vector of NaNs.

mnist/logistic_tf.py
@@ -13,7 +13,7 @@ one for each digit in 0..9.
 """
 INPUT_DIMENSIONS  = 784
 OUTPUT_DIMENSIONS = 10
-LEARNING_RATE     = 0.5
+LEARNING_RATE     = 0.05
 BATCH_SIZE        = 100
 
 sess = tf.Session()
@@ -31,10 +31,10 @@ sess.run(tf.initialize_all_variables())
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Bayes classifiers
-classifier = tf.argmax(y,1)
+predict = tf.argmax(y,1)
 
 # Risk estimators
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y + 1e-10), reduction_indices=[1]))
 
 train_step = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE).minimize(cross_entropy)
 
@@ -43,12 +43,12 @@ mnist_train = mnist.from_data_frame(mnist.fetch_data(mnist.TRAIN))
 
 for (labels, images) in mnist.batches(mnist_train, batch_size=BATCH_SIZE):
     train_step.run(feed_dict={x: images, y_: labels}, session=sess)
-
+ 
 # Run the model on the testing data.
 mnist_test = mnist.from_unlabeled_data_frame(mnist.fetch_data(mnist.TEST))
 
 for batch in mnist.unlabeled_batches(mnist_test, batch_size=BATCH_SIZE):
-    sess.run(classifier, feed_dict={x: batch})
+    print(sess.run(predict, feed_dict={x: batch}))
 
 
 sess.close()