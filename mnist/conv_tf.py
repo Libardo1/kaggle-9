e8755b32804e8f145c28b689782f61b1afbca189
Stallmanifold <stallmanifold@gmail.com>
2016-07-14 18:00:57 -0500

    Decomposed convnet script into functions.

--- mnist/conv_tf.py
+++ mnist/conv_tf.py
@@ -1,4 +1,26 @@
+"""
+Convnet classifier for MNIST data using TensorFlow.
+"""
 import tensorflow as tf
+
+
+DEFAULT_BATCH_SIZE = 50
+
+
+class ConvNetClassifier():
+    def __init__(self, 
+                 objective_func, 
+                 train_step, 
+                 correct_prediction, 
+                 accuracy_metric, 
+                 output_layer):
+
+        self.objective_func = objective_func
+        self.train_step = train_step
+        self.correct_prediction = correct_prediction
+        self.accuracy_metric = accuracy_metric
+        self.output_layer = output_layer
+
 
 
 def weight_variable(shape):
@@ -20,39 +42,96 @@ def max_pool_2x2(x):
                           strides=[1,2,2,1], padding='SAME')
 
 
-"""
-First layer
-"""
-W_conv1 = weight_variable([5,5,1,32])
-b _conv1 = bias_variable([32])
+def conv_hlayer1(x):
+    """
+    First hidden layer of convolutional neural net.
+    """
+    W_conv1 = weight_variable([5,5,1,32])
+    b_conv1 = bias_variable([32])
 
-x_image = tf.reshape(x, [-1,28,28,1])
+    x_image = tf.reshape(x, [-1,28,28,1])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    #Output of hidden layer.
+    h_pool1 = max_pool_2x2(h_conv1)
 
-"""
-Second layer
-"""
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+    return h_pool1
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
 
-"""
-Third layer
-"""
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+def conv_hlayer2(h_pool1):
+    """
+    Second hidden layer of convolutional neural net.
+    """
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    return h_pool2
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+def conv_hlayer3(h_pool2):
+    """
+    Third hidden layer of convolutional neural net with dropout.
+    """
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    return h_fc1_drop
+
+
+def conv_output_layer(h_fc1_drop):
+    """
+    Readout layer
+    """
+    W_fc2  = weight_variable([1024, 10])
+    b_fc2  = bias_variable([10])
+
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    return y_conv
+
+
+def build_nn(x):
+    """
+    Construct the convolutional neural network and return the output layer variable.
+    """
+    h_pool1 = conv_hlayer1(x)
+    h_pool2 = conv_hlayer2(h_pool1)
+    h_fc1_drop = conv_hlayer3(h_pool2)
+    y_conv = conv_output_layer(h_fc1_drop)
+
+    return y_conv
+
+# Convnet to be used for classifying MNIST digits.
+y_conv = build_nn(x)
+
+# Objective function.
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+# Training step.
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess.run(tf.initialize_all_variables())
+
+for i in range(20000):
+  batch = mnist.train.next_batch(DEFAULT_BATCH_SIZE)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+accuracy_eval = accuracy.eval(feed_dict={x: mnist.test.images, 
+                                         y_: mnist.test.labels, keep_prob: 1.0})
+
+print("test accuracy {}".format(accuracy_eval))
